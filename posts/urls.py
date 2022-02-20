0f12015c1c415fcc8e94e1f303712c770fa7203c
awesomedev08 <awesomedev08@gmail.com>
2022-02-20 14:51:30 +0700

    Add: Show List categories, and show posts by categories

--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    re_path('detail/(?P<slug>[\w-]+)$', views.BlogDetail.as_view(), name='detail_blog'),
+    re_path('detail/(?P<slug>[\w\d-]+)$', views.BlogDetail.as_view(), name='detail_blog'),
+    re_path('category/(?P<slug>[\w\d-]+)$', views.PostCategories.as_view(), name='category'),
+    path('list-categories', views.ListCategories.as_view()),
     path('', views.BlogList.as_view(), name='bloglist')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
+from re import template
 from django.views.generic import DetailView, ListView
-from .models import Post
+from .models import Post, Category
 
 class BlogDetail(DetailView):
     model = Post 
@@ -10,3 +11,22 @@ class BlogList(ListView):
     template_name = 'posts/index.html'
     context_object_name = 'posts'
     model = Post 
+
+class PostCategories(ListView):
+    """
+    Show Posts by Category
+    """
+    template_name = 'posts/index.html'
+    context_object_name = 'posts'
+    model = Post 
+    
+    def get_queryset(self):
+        return self.model.objects.filter(categories__name=self.kwargs.get('slug').title()).all()
+
+class ListCategories(ListView):
+    template_name = 'posts/list-categories.html'
+    model = Category
+    context_object_name = 'categories'
+
+    def get_queryset(self):
+        return self.model.objects.all().order_by('-name').reverse()
