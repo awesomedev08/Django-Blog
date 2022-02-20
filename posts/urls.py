5145fbf12e086f1bfc6d952468d3fb8ab597eeb4
awesomedev08 <awesomedev08@gmail.com>
2022-02-20 14:56:39 +0700

    Add: url name

--- posts/urls.py
+++ posts/urls.py
@@ -7,6 +7,6 @@ app_name = 'blog'
 urlpatterns = [
     re_path('detail/(?P<slug>[\w\d-]+)$', views.BlogDetail.as_view(), name='detail_blog'),
     re_path('category/(?P<slug>[\w\d-]+)$', views.PostCategories.as_view(), name='category'),
-    path('list-categories', views.ListCategories.as_view()),
+    path('list-categories', views.ListCategories.as_view(), name='list_categories'),
     path('', views.BlogList.as_view(), name='bloglist')
 ]
